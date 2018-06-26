2876c07b5932d68d276287533b9b14f1ee866f60
Jesse McReady <jessemcready@gmail.com>
2018-06-26 00:03:25 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,3 +2,15 @@
 def word_substituter(tweet)
     
 end
+
+def bulk_tweet_shortener(tweets)
+  
+end
+
+def selective_tweet_shortener(tweet)
+  
+end
+
+def shortened_tweet_truncator(tweet)
+  
+end
