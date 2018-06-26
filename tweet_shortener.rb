bdda395fbbac274c1056a749c86b7eca3148e73f
Jesse McReady <jessemcready@gmail.com>
2018-06-26 00:21:21 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -42,7 +42,9 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  
+  if tweet.length > 140
+    word_substituter(tweet)
+  end
 end
 
 def shortened_tweet_truncator(tweet)
